2f998ae7a8256fafeef56b64626a3f3f4c6c4ed4
Marian COJOC ro <marian.cojoc.ro@gmail.com>
2016-06-02 14:47:55 +0300

    some work on rx version

--- app/modifiers-rx.js
+++ app/modifiers-rx.js
@@ -1,12 +1,25 @@
+const Rx = require('rx');
+const originalModifiers = require('./modifiers');
+
 const events = (state) => {
+  const keydownStream = Rx.Observable.fromEvent(document, 'keydown');
+  const aStream = keydownStream.filter((event) => {
+    return event.key === 'a';
+  });
+  const wStream = keydownStream.filter((event) => {
+    return event.key === 'w';
+  });
 
+  Rx.Observable.when(aStream.and(wStream).thenDo((a,b)=>{
+    console.log(a.key,b.key);
+    return 'test';
+  })).subscribe((event) => {
+    console.log('when', event);
+  });
 };
 
-const update = (state, diff) => {
-  return state;
-};
 
 module.exports = {
   events,
-  update
-}
+  update: originalModifiers.update
+};
